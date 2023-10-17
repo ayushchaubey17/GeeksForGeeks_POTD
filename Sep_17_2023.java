a44ba641308ad39e392d380b32497eaf618380d8
Ayush <ayushreso1234@gmail.com>
2023-10-17 21:35:26 +0530

    ayush

--- Sep_17_2023.java
+++ Sep_17_2023.java
@@ -1,8 +1,48 @@
 // Transitive closure of a graph....
 
-public class Sep_17_2023 {
-    public static void main(String[] args) {
-        System.out.println("i am ayush");
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+class Solution{
+
+    static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][])
+    {
+        ArrayList<ArrayList<Integer>> transitive = new ArrayList<>();
+        for (int i = 0; i < N; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                temp.add(0);
+            }
+            transitive.add(temp);
+        }
+
+
+        for (int i = 0; i < N; i++){
+            boolean[] visited = new boolean[N];
+            Arrays.fill(visited, false);
+
+            ArrayList<Integer> pathFrom = new ArrayList<>();
+            dfs(transitive, pathFrom, i, graph, visited);
+
+            for (int node: pathFrom) {
+                transitive.get(i).set(node, 1);
+            }
+        }
+
+        return transitive;
+
     }
 
+
+    static void dfs(ArrayList<ArrayList<Integer>> transitive, ArrayList<Integer> pathFrom, int node, int[][] graph, boolean[] visited) {
+        visited[node] = true;
+        pathFrom.add(node);
+
+        for (int nbr = 0; nbr < graph[node].length; nbr++) {
+            if (!visited[nbr] && graph[node][nbr] == 1)
+                dfs(transitive, pathFrom, nbr, graph, visited);
+        }
+
+    }
 }
